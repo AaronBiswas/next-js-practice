f9b56cf82b9c91e359a7fdf4cecb5aa86b90b729
Aaron Biswas <savybiswas15@gmail.com>
2025-10-12 20:17:37 +0530

    Nested dynamic routes done

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default async function ReviewDetails({
+  params,
+}: {
+  params: Promise<{ productId: string; reviewId: string }>;
+}) {
+  const { productId, reviewId } = await params;
+  return (
+    <div>
+      <h1>
+        Product details: Product ID: {productId} Review ID:{reviewId}
+      </h1>
+    </div>
+  );
+}

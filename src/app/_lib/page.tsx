34e5ba5c23b67113916b43b0a470fbb7dc2d2a2f
Aaron Biswas <savybiswas15@gmail.com>
2025-10-14 15:27:55 +0530

    Private folder structure done

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+//This is the implementation of private folders that use _ infront of their names to avoid being a route in nextjs
+export default function Page() {
+  return <div>This page will never show up</div>;
+}

bdfeb5ea69a6ac519302f1451e6263aaeb7c9991
Aaron Biswas <savybiswas15@gmail.com>
2025-11-08 13:34:30 +0530

    loading state done

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-export default function Page() {
+export default async function Page() {
+  await new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve("Intentional Delay for loading.tsx")
+    },3000)
+  })
   return (
     <div>
         <h1>Blog Page</h1>

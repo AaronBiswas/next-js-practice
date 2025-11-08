bcb35fc2f3ab41ccb822bc438f887b35fb6fed4a
Aaron Biswas <savybiswas15@gmail.com>
2025-11-08 13:51:10 +0530

    error handling component done

---
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,25 @@
+"use client"; //Make sure the error page is always use client
+import { useRouter } from "next/navigation";
+import React, { startTransition } from "react";
+
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+  return (
+    <div>
+      <h1>{error.message}</h1>
+      <button onClick={reload}>Try again</button>
+    </div>
+  );
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,22 @@
-import { notFound, redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 import React from "react";
+
+
+function getRandomInt(count:number){
+  return Math.floor(Math.random()*count)  //generates random number
+}
 
 export default async function ReviewDetails({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
+  const random=getRandomInt(2)
+
+  if(random==1){
+    throw new Error("Error loading review")   //added to check error.tsx
+  }
+
   const { productId, reviewId } = await params;
   if (parseInt(reviewId) > 1000) {
     // notFound();
